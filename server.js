9b6d931e791615ca6064c3b98166d8c04033b898
Adam Calcasola <adam.calcasola@gmail.com>
2022-02-13 17:49:13 -0500

    fixes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
