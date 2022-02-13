314a977dc49c41e97937457e26fe1186efaa3a63
Adam Calcasola <adam.calcasola@gmail.com>
2022-02-13 18:14:22 -0500

    double quotes to single quotes

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 // const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budget";
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', 
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
